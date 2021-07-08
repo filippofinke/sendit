99495822e9c6f6909479e72f1f86293e2eed20f4
Filippo Finke <filippofinke@protonmail.ch>
2021-07-08 12:02:59 +0200

    :sparkles: Added MAX_FILE_SIZE

--- index.js
+++ index.js
@@ -5,40 +5,51 @@ const JSONdb = require("simple-json-db");
 const nanoid = require("nanoid").nanoid;
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
+const MAX_FILE_SIZE = process.env.MAX_FILE_SIZE || 1024 * 1024 * 20; // 20MB
 
 const db = new JSONdb(path.join(__dirname, "database.json"));
 
 app.use(express.static("public"));
 
 app.get("/:id", (req, res) => {
-  let id = req.params.id;
+	let id = req.params.id;
 
-  let file = db.get(id);
-  if (file) {
-    return res.download(path.join(__dirname, "files", id), file.name);
-  } else {
-    return res.sendStatus(404);
-  }
+	let file = db.get(id);
+	if (file) {
+		return res.download(path.join(__dirname, "files", id), file.name);
+	} else {
+		return res.sendStatus(404);
+	}
 });
 
 app.post("/:name", (req, res) => {
-  let id = nanoid();
-  let file = fs.createWriteStream(path.join(__dirname, "files", id));
+	let id = nanoid();
+	let file = fs.createWriteStream(path.join(__dirname, "files", id));
 
-  req
-    .on("data", (data) => {
-      file.write(data);
-    })
-    .on("end", () => {
-      db.set(id, {
-        name: req.params.name,
-        createdAt: Date.now(),
-      });
-      res.send(id);
-    });
+	let size = 0;
+	req
+		.on("data", (data) => {
+			size += data.length;
+			if (size > MAX_FILE_SIZE) {
+				file.close();
+				req.pause();
+				fs.unlink(path.join(__dirname, "files", id), () => {
+					res.status(400).send("The maximum file size is " + MAX_FILE_SIZE + " bytes!");
+				});
+			} else {
+				file.write(data);
+			}
+		})
+		.on("end", () => {
+			db.set(id, {
+				name: req.params.name,
+				createdAt: Date.now(),
+			});
+			res.send(id);
+		});
 });
 
-app.listen(port, () => {
-  console.log(`send-it server listening on http://localhost:${port}`);
+app.listen(PORT, () => {
+	console.log(`send-it server listening on http://localhost:${PORT}`);
 });
