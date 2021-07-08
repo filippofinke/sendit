21b76c769c28b8ce9db34ad80e66970a618365b1
Filippo Finke <filippofinke@protonmail.ch>
2021-07-08 09:49:29 +0200

    :recycle: Code refactor

--- index.js
+++ index.js
@@ -1,75 +1,40 @@
 require("dotenv").config();
-const express = require("express");
-const Busboy = require("busboy");
 const fs = require("fs");
-const { nanoid } = require("nanoid");
+const path = require("path");
+const JSONdb = require("simple-json-db");
+const nanoid = require("nanoid").nanoid;
+const express = require("express");
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 8080;
 
-let files = [];
+const db = new JSONdb(path.join(__dirname, "database.json"));
 
-app.use(express.static("public"));
-
-app.get("/api/:id/download", (req, res) => {
+app.get("/:id", (req, res) => {
   let id = req.params.id;
 
-  if (files[id]) {
-    return res.download(__dirname + "/files/" + id, files[id].name);
+  let file = db.get(id);
+  if (file) {
+    return res.download(path.join(__dirname, "files", id), file.name);
   } else {
     return res.sendStatus(404);
   }
 });
 
-app.get("/api/:id", (req, res) => {
-  let id = req.params.id;
-
-  if (files[id]) {
-    return res.send(files[id]);
-  } else {
-    return res.sendStatus(404);
-  }
-});
-
-app.post("/api/", (req, res) => {
+app.post("/:name", (req, res) => {
   let id = nanoid();
+  let file = fs.createWriteStream(path.join(__dirname, "files", id));
 
-  while (files[id]) id = nanoid();
-
-  console.log(`# Reserved ${id}`);
-  files[id] = {
-    id: id,
-    createdAt: new Date().getTime(),
-  };
-
-  return res.send(id);
-});
-
-app.post("/api/:id", (req, res) => {
-  let id = req.params.id;
-
-  if (files[id]) {
-    if (files[id].uploaded) return res.sendStatus(400);
-
-    let busboy = new Busboy({ headers: req.headers });
-
-    busboy.on("file", (fieldname, file, filename, encoding, mimetype) => {
-      files[id].name = filename;
-      file.pipe(fs.createWriteStream(__dirname + "/files/" + id));
+  req
+    .on("data", (data) => {
+      file.write(data);
+    })
+    .on("end", () => {
+      db.set(id, {
+        name: req.params.name,
+        createdAt: Date.now(),
+      });
+      res.send(id);
     });
-
-    busboy.on("finish", () => {
-      files[id].uploaded = true;
-      res.send(files[id]);
-    });
-
-    return req.pipe(busboy);
-  } else {
-    return res.sendStatus(404);
-  }
-});
-
-app.get("(/*)?", async (req, res, next) => {
-  res.sendFile(__dirname + "/public/index.html");
 });
 
 app.listen(port, () => {
