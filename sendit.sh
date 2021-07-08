0c6835b2da143ff920550ad1b513ea5be3fd79c0
Filippo Finke <filippofinke@protonmail.ch>
2021-07-08 10:16:09 +0200

    :sparkles: Added upload progress

--- sendit.sh
+++ sendit.sh
@@ -14,5 +14,6 @@ fi
 FILE_PATH=$1
 FILE_NAME="$(basename -- $FILE_PATH)"
 echo "Uploading $FILE_PATH as $FILE_NAME"
-ID=`cat $FILE_PATH | curl -X POST --data-binary @- "$SENDIT_API/$FILE_NAME"`
+
+ID=$(curl -X POST --data-binary @$FILE_PATH "$SENDIT_API/$FILE_NAME")
 echo "Done, ID: $ID"
